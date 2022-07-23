3f193ae8d7d213a6cb3902af46e5cc7ded815430
xingpancanada <xingpancanada@hotmail.com>
2022-07-23 16:16:40 -0400

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Component, OnInit } from '@angular/core';
+import { AppState } from './Interfaces/AppState';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'MyNgrx';
+
+  counter$?: Observable<number>;
+
+ //14
+  constructor(
+    private store: Store<AppState>
+  ){
+    this.counter$ = store.select('counter');
+  }
+
+  ngOnInit(): void {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,55 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { StoreModule } from '@ngrx/store';
+
+// if want to set Action type, we can use below
+// export interface ActionWithPayload<T> extends Action {
+//   payload: T;
+// }
+
+const counterReducer = (state = 0, action: any) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+////14.15
+const productsReducer = (state = [{}], action: any) => {
+  switch (action.type) {
+    case 'ADD':
+      return [ ...state, {...action.payload}];
+    case 'REMOVE':
+      return state.filter((p: any) => p.id !== action.payload.id);
+    case 'UPDATE':
+      return state.map((p:any) => {
+        if(p.id === action.payload.id){
+          return {...p, ...action.payload};
+        }else{
+          return p;
+        }
+      });
+    default:
+      return state;
+  }
+};
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //14
+    StoreModule.forRoot({
+      counter: counterReducer,
+      products: productsReducer
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
